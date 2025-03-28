e108d58411f51398f6aa02093da1217506fe91e1
Matthew Jones <matthew.jones561@gmail.com.com>
2025-03-28 19:29:06 -0400

    1st attempt at Parser. Doesnt compile yet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+/*
 fn main() {
     println!("Hello, world!");
+}
+*/
+mod parser; // This tells Rust to include src/parser.rs as a module.
+use parser::Parser; // Now we can use Parser here.
+
+fn main() {
+    let inputs = [
+        "42",
+        "True",
+        r#""Hello, world!""#,
+        "struct { x: 42, y: 7 }",
+        "Option::Some(42)",
+        "x -> { x }",
+    ];
+
+    for input in &inputs {
+        let mut parser = Parser::new(input);
+        match parser.parse_expr() {
+            Ok(expr) => println!("Input: {}\nParsed AST: {:?}\n", input, expr),
+            Err(e) => println!("Input: {}\nError: {}\n", input, e),
+        }
+    }
 }
